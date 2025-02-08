d8c6943bc6383a2495e6423b2040d29d68846b85
SakthivelanPandy <sakthivelanpandy@gmail.com>
2025-02-08 11:59:42 +0000

    try and add animation

---
+++ scripts/ingredients_tween.gd
@@ -0,0 +1,22 @@
+extends Node
+
+
+func animate_ingredient(target_node: Node2D, target_position: Vector2, animation_duration: float):
+	print("animation")
+	var tween = create_tween()
+	var sprite = target_node.get_node("Sprite2D")
+	
+	tween.parallel().tween_property(sprite, "scale", Vector2(0, 0), animation_duration)
+	# Animate position
+	tween.parallel().tween_property(sprite, "position", target_position*(-1), animation_duration)
+
+	# Animate scale (shrink to zero)
+
+
+	# Animate rotation (rotate 180 degrees)
+	tween.parallel().tween_property(target_node, "rotation", deg_to_rad(180), animation_duration)
+
+	# Optional: wait for the animation duration to finish and then remove the object
+	await get_tree().create_timer(animation_duration).timeout
+	target_node.queue_free()
+	
